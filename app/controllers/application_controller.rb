ba975520c66c3a0b93e519c2047dba1a43f02f63
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-02-25 09:10:51 +0000

    [Add] 26

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
+before_action :configure_permitted_parameters, if: :devise_controller?
 
 def after_sign_in_path_for(resource)
   books_path
@@ -14,5 +15,6 @@ end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
